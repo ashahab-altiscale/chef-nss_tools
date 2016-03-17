5379f04070ceafb17152f6c30d1e9819e359e99a
Travis Thompson <travis@altiscale.com>
2016-03-17 13:47:31 -0700

    Fix chefspec matchers

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,6 +1,6 @@
 if defined?(ChefSpec)
   def update_nss_tools_certdb(name)
-    ChefSpec::Matchers::ResourceMatcher.new(:nss_tools_certdb, :write, name)
+    ChefSpec::Matchers::ResourceMatcher.new(:nss_tools_certdb, :update, name)
   end
 
   def delete_nss_tools_certdb(name)
